aae55a6d08bc4d7754b78d5dcd95b59aba459d4e
Vitor Costa <vitor.costa@exaud.com>
2022-08-26 11:21:30 +0100

    part2-added interactivity to the icons

--- lib/main.dart
+++ lib/main.dart
@@ -50,16 +50,24 @@ class _RamdomWordsState extends State<RamdomWords> {
         }
         final alreadySaved = _saved.contains(_suggestions[index]);
         return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-          ),
-        );
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            });
       },
     );
   }
