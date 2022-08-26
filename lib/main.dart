514e4ab068dbaffbfe3ad491881c16e478ae7a24
Vitor Costa <vitor.costa@exaud.com>
2022-08-26 12:52:26 +0100

    part2-changed UI appBar theme/colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
-      home: RamdomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.redAccent,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const RamdomWords(),
     );
   }
 }
