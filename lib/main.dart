568a7e8a4deb5bb180ffcab4ff96530d855221d1
Vitor Costa <vitor.costa@exaud.com>
2022-08-26 11:01:17 +0100

    part2-added heart/favorite icons

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class RamdomWords extends StatefulWidget {
 
 class _RamdomWordsState extends State<RamdomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -47,10 +48,16 @@ class _RamdomWordsState extends State<RamdomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
+        final alreadySaved = _saved.contains(_suggestions[index]);
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
           ),
         );
       },
